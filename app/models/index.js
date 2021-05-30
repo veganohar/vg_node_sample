3c37617001f37660d60f2be8e5ab27f66ce08dc4
Manohar Choudary <manoharchoudaryv@gmail.com>
2021-05-30 18:29:37 +0530

    db connection

---
+++ app/models/index.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+const db = {};
+db.mongoose = mongoose;
+
+module.exports = db;
--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const bodyParser = require("body-parser");
+const db = require("./app/models");
+const dbconfig = require("./app/config/db.config");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+
 app.listen(PORT,()=>{
     console.log("Sample Node project is running at http://localhost:"+PORT);
 })
@@ -14,5 +17,16 @@ app.get("/",(req,res)=>{
     res.send("Welcome to Node Js");
 })
 
+db.mongoose.connect(`mongodb://${dbconfig.HOST}:${dbconfig.PORT}/${dbconfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  }).then(()=>{
+      console.log("Connected to DB successfully");
+  }).catch((err)=>{
+    console.log(err);
+    process.exit();
+  })
 
 require("./app/routes/test.route")(app);
